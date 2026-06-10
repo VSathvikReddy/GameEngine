5902cac7c4637d876467bc77a3b1e87916548075
VSathvikReddy <vangapellisathvik@gmail.com>
2026-06-10 17:03:59 +0530

    Redoing

--- include/ECS/entity.hpp
+++ include/ECS/entity.hpp
@@ -2,34 +2,20 @@
 
 #include <cstdint>
 #include <bitset>
-#include <queue>
-#include <array>
-
 
 using Entity = uint32_t;
 const Entity MAX_ENTITIES = 5000;
 
-using ComponentType = uint8_t;
+using ComponentType = std::uint8_t;
 const ComponentType MAX_COMPONENTS = 32;
 
 using Signature = std::bitset<MAX_COMPONENTS>;
 
-
-
 class EntityManager{
 public:
-	EntityManager();
 
-	Entity createEntity();
-	Entity createEntity(Signature signature);
-	void destroyEntity(Entity entity);
 
-	void setSignature(Entity entity, Signature signature);
-	Signature getSignature(Entity entity);
 
 private:
-	std::queue<Entity> mAvailableEntities;
-	std::array<Signature, MAX_ENTITIES> mSignatures;
-
-	uint32_t mLivingEntityCount = 0;
+    
 };
--- include/ECS/transform.hpp
+++
@@ -1,8 +0,0 @@
-#include "Geometry/vector2.hpp"
-#include "Geometry/angle.hpp"
-
-struct TransformComponent{
-	Vec2f position;
-	AngleF rotation;
-	Vec2f scale;
-};
