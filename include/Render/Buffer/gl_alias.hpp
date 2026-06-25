88109f92e7af8cd09d8b717bbeb71710345a3e57
VSathvikReddy <vangapellisathvik@gmail.com>
2026-06-26 01:23:06 +0530

    Re making render, shader, texture

--- include/Render/Buffer/gl_alias.hpp
+++ include/Render/Buffer/gl_alias.hpp
@@ -2,18 +2,26 @@
 
 #include <cstdint>
 
-using ShaderID  = uint32_t;
-using ShaderGPUID = uint32_t;
-constexpr ShaderID ERROR_SHADER = 0;
-constexpr ShaderID NO_SHADER     = 0;
+using ShaderID      = uint32_t;
+using ShaderGPUID   = uint32_t;
+constexpr ShaderID ERROR_SHADER     = 0;
+constexpr ShaderID NO_SHADER        = 0;
 
 
-using TextureID = uint32_t;
-constexpr TextureID ERROR_TEXTURE = 0;
+using TextureID     = uint32_t;
+using TextureGPUID  = uint32_t;
+constexpr TextureID ERROR_TEXTURE   = 0;
 
 
 
-using VaoID     = uint32_t;
-using VboID     = uint32_t;
-using EboID     = uint32_t;
-using UboID     = uint32_t;
+using VaoID         = uint32_t;
+using VaoGPUID      = uint32_t;
+
+using VboID         = uint32_t;
+using VboGPUID      = uint32_t;
+
+using EboID         = uint32_t;
+using EboGPUID      = uint32_t;
+
+using UboID         = uint32_t;
+using UboGPUID      = uint32_t;
